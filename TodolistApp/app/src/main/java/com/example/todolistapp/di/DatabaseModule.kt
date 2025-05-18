057a688c71fbfd57d519c1eaaa851b347e79d568
abdu <abdualkerim01@gmail.com>
2025-05-18 19:11:35 +0300

    Added the dependancy injection

---
+++ TodolistApp/app/src/main/java/com/example/todolistapp/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.example.todolistapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.todolistapp.data.local.TodoDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(
+        @ApplicationContext context: Context
+    ): TodoDatabase {
+        return Room.databaseBuilder(
+            context,
+            TodoDatabase::class.java,
+            "todo_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoDao(database: TodoDatabase) = database.todoDao()
+} 
